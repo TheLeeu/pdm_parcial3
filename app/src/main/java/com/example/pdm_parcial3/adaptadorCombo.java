d995f657eb1fb94031e3231644850ed62d314901
TheLeeu <arandaantonio148@gmail.com>
2022-06-07 15:42:47 -0600

    Se agrego la carga de imagenes en el adaptadorCombo traidas con la url de firebase

--- app/src/main/java/com/example/pdm_parcial3/adaptadorCombo.java
+++ app/src/main/java/com/example/pdm_parcial3/adaptadorCombo.java
@@ -5,9 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.pdm_parcial3.Entidad.Combo;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -16,9 +20,14 @@ public class adaptadorCombo extends BaseAdapter {
     public ArrayList<Combo> data;
     private Context context;
 
+    public FirebaseStorage storage;
+    public StorageReference reference;
+
     public adaptadorCombo(ArrayList<Combo> c, Context con){
         this.data = c;
         this.context = con;
+        storage = FirebaseStorage.getInstance();
+        reference = storage.getReference();
     }
     @Override
     public int getCount() {
@@ -45,12 +54,16 @@ public class adaptadorCombo extends BaseAdapter {
         TextView plato = (TextView) view.findViewById(R.id.textView3);
         TextView postre = (TextView) view.findViewById(R.id.textView4);
         TextView precio = (TextView) view.findViewById(R.id.textView5);
+        ImageView img = (ImageView) view.findViewById(R.id.imageView2);
 
         numero.setText("Numero: "+c.numCombo);
         entrada.setText("Entrada: "+c.entrada);
         plato.setText("Plato: "+c.platoFuerte);
         postre.setText("Postre: "+c.postre);
         precio.setText("Precio: "+c.precio);
+
+        Glide.with(context).load(c.img).into(img);
+
 
         return view;
     }
